747fdd2eca826da1497cbe5814b697d3eb012225
mackpounds <igiliegbepatrick@gmail.com>
2023-01-01 02:06:47 +0100

    arrangments

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,7 +5,18 @@ import './App.css';
 function App() {
     // For the form indexes
     const [step, setSteps] = useState(0)
+    // for Next | back buttons
     const [direction, setDirection] = useState<"next" | "back" | null>(null);
+
+    // useState for each Input
+    const [coinName, setCoinName] = useState("");
+    const [holdings, setHoldings] = useState("");
+    const [upThreshold, setUpThreshold] = useState("")
+    const [downThreshold, setDownThreshold] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+    const [userConfirmPass, setUserConformPass] = useState("")
+
 
 
     const nextStep = () => {
@@ -32,20 +43,54 @@ function App() {
 
             {step === 0 && (
           <div className={getClass(0)}>
-               <label htmlFor="coin-name" ></label>
-              <input type="text" id="coin-name" placeholder="Enter your Coin" className="coin-name" />
-              <button onClick={nextStep} className="btn-first-action" >Continue <i className="fa-solid fa-chevron-right"></i></button>
+               <label
+                   htmlFor="coin-name" >
+               </label>
+              <input
+                  type="text"
+                  id="coin-name"
+                  value={coinName}
+                  onChange={(e) => setCoinName(e.target.value)}
+                  placeholder="Enter your Coin"
+                  className="coin-name"
+              />
+              <button
+                  onClick={nextStep}
+                  className="btn-first-action">
+                  Continue
+                  <i className="fa-solid fa-chevron-right"></i>
+              </button>
           </div>
             )}
 
 
             {step === 1 && (
           <div className={getClass(1)}>
-              <label htmlFor="holdings" className="label-holding-coin">How much coin are you holding?
-              <input type="number" name="holdings" placeholder="Coin amount" id="holdings" /></label>
+              <label
+                  htmlFor="holdings"
+                  className="label-holding-coin">
+                  How much coin are you holding?
+              <input
+                  type="number"
+                  name="holdings"
+                  value={holdings}
+                  onChange={(e) => setHoldings(e.target.value)}
+                  placeholder="Coin amount"
+                  id="holdings"
+              /></label>
               <div className="btn-section">
-              <button onClick={prevStep} className="go-back-btn"><i className="fa-solid fa-chevron-left"></i> back </button>
-              <button onClick={nextStep} className="move-from-holdings">Next <i className="fa-solid fa-chevron-right"></i></button>
+              <button
+                  onClick={prevStep}
+                  className="go-back-btn">
+                  <i className="fa-solid fa-chevron-left"></i>
+                  back
+              </button>
+              <button
+                  onClick={nextStep}
+                  className="move-from-holdings">
+                  Next
+                  <i className="fa-solid fa-chevron-right"></i>
+              </button>
               </div>
           </div>
                 )}
@@ -53,11 +98,26 @@ function App() {
             {step === 2 && (
             <div className={getClass(2)}>
                 <div className="price-update">
-                    <span className="price-alert">Your <code>coin</code> current price is now <code>$2000</code></span>
+                    <span className="price-alert">
+                        Your <code>coin</code> current price is now <code>$2000</code>
+                    </span>
                 </div>
                 <div className="btn-section">
-                  <button onClick={prevStep} className="go-back-btn"><i className="fa-solid fa-chevron-left"></i> back </button>
-                  <button onClick={nextStep} className="move-from-holdings">Next <i className="fa-solid fa-chevron-right"></i></button>
+                  <button
+                      onClick={prevStep}
+                      className="go-back-btn">
+                      <i className="fa-solid fa-chevron-left"></i>
+                      back
+                  </button>
+                  <button
+                      onClick={nextStep}
+                      className="move-from-holdings">
+                      Next
+                      <i
+                          className="fa-solid fa-chevron-right">
+
+                      </i>
+                  </button>
               </div>
             </div>
                 )}
@@ -65,18 +125,49 @@ function App() {
 
             {step === 3 && (
             <div className={getClass(3)}>
-                <label htmlFor="up-threshold" className="label-threshold-up">
+                <label
+                    htmlFor="up-threshold"
+                    className="label-threshold-up">
                     Enter the Upper price threshold:
-                    <input type="number" name="up-threshold" placeholder="0.5000" id="up-threshold"/>
+                    <input
+                        type="number"
+                        name="up-threshold"
+                        value={upThreshold}
+                        onChange={(e) => setUpThreshold(e.target.value)}
+                        placeholder="0.5000"
+                        id="up-threshold"
+                    />
                 </label>
-                <label htmlFor="down-threshold" className="label-threshold-down">
+                <label
+                    htmlFor="down-threshold"
+                    className="label-threshold-down">
                     Enter the Low price threshold:
-                    <input type="number" name="down-threshold" placeholder="0.4500" id="down-threshold"/>
+                    <input
+                        type="number"
+                        name="down-threshold"
+                        value={downThreshold}
+                        onChange={(e) => setDownThreshold(e.target.value)}
+                        placeholder="0.4500"
+                        id="down-threshold"
+                    />
                 </label>
-                <p className="threshold-info"><i className="fa-solid fa-info-circle"></i>  Set the price limit at which you want to be notified when your price goes above or below.</p>
+                <p className="threshold-info">
+                    <i className="fa-solid fa-info-circle"></i>
+                    Set the price limit at which you want to be notified when your price goes above or below.
+                </p>
                 <div className="btn-section">
-              <button onClick={prevStep} className="go-back-btn"><i className="fa-solid fa-chevron-left"></i> back </button>
-              <button onClick={nextStep} className="move-from-holdings">Next <i className="fa-solid fa-chevron-right"></i></button>
+              <button
+                  onClick={prevStep}
+                  className="go-back-btn">
+                  <i className="fa-solid fa-chevron-left"></i>
+                  back
+              </button>
+              <button
+                  onClick={nextStep}
+                  className="move-from-holdings">
+                  Next
+                  <i className="fa-solid fa-chevron-right"></i>
+              </button>
               </div>
             </div>
                 )}
@@ -84,14 +175,43 @@ function App() {
 
             {step === 4 && (
             <div className={getClass(4)}>
-                <label htmlFor="user-email" className="label-email">Enter your email & password
-                    <input type="email" name="user-email" placeholder="a@example.com" id="user-email" aria-autocomplete="inline" />
+                <label
+                    htmlFor="user-email"
+                    className="label-email">
+                    Enter your email & password
+                    <input
+                        type="email"
+                        name="user-email"
+                        value={userEmail}
+                        onChange={(e) => setUserEmail(e.target.value)}
+                        placeholder="a@example.com"
+                        id="user-email"
+                        aria-autocomplete="inline"
+                    />
                 </label>
-                <label htmlFor="user-password" className="label-password">
-                    <input type="password" name="user-password" placeholder="Password" id="user-password" />
+                <label
+                    htmlFor="user-password"
+                    className="label-password">
+                    <input
+                        type="password"
+                        name="user-password"
+                        value={userPassword}
+                        onChange={(e) => setUserPassword(e.target.value)}
+                        placeholder="Password"
+                        id="user-password"
+                    />
                 </label>
-                <label htmlFor="user-confirm-pass" className="label-confirm-pass">
-                    <input type="password" name="user-confirm-pass" placeholder="Confirm password" id="user-confirm-pass" />
+                <label
+                    htmlFor="user-confirm-pass"
+                    className="label-confirm-pass">
+                    <input
+                        type="password"
+                        name="user-confirm-pass"
+                        value={userConfirmPass}
+                        onChange={(e) => setUserConformPass(e.target.value)}
+                        placeholder="Confirm password"
+                        id="user-confirm-pass"
+                    />
                 </label>
                 <div className="btn-section">
                 <button onClick={prevStep} className="go-back-btn"><i className="fa-solid fa-chevron-left"></i> back </button>
