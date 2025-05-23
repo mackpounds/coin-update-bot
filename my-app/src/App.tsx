10e13b5fccc677151411a6dc6ecc2c9a4763d965
mackpounds <igiliegbepatrick@gmail.com>
2025-05-23 13:47:34 +0100

    flexible changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,40 +3,55 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    // For the form indexes
     const [step, setSteps] = useState(0)
+    const [direction, setDirection] = useState<"next" | "back" | null>(null);
+
+
     const nextStep = () => {
-        setSteps((prev) => prev + 1);
+        setDirection("next")
+        setSteps(prev => Math.max(prev + 1, 0));
     };
     const prevStep = () => {
-        setSteps((prev) => prev - 1);
+        setDirection("back")
+        setSteps(prev => Math.max(prev - 1, 0));
     };
+
+    const getClass = (index: number) => {
+        if (index === step) return `parent next ${direction ===  "back" ? "back": ""}`
+        return "parent";
+    }
+
+
   return (
     <header className="app_header">
       <div className="main-container">
         <h2 className="App-name">Coin Tracker</h2>
         <p className="sub-text">Let cTracker update you for YOU!</p>
-        <form action="/" className="coin-input-content">
+        <div  className="coin-input-content">
+
             {step === 0 && (
-          <div className="coin-name-layout">
+          <div className={getClass(0)}>
                <label htmlFor="coin-name" ></label>
-              <input type="text" id="coin-name" placeholder="Enter your Coin" className="coin-name" required/>
+              <input type="text" id="coin-name" placeholder="Enter your Coin" className="coin-name" />
               <button onClick={nextStep} className="btn-first-action" >Continue <i className="fa-solid fa-chevron-right"></i></button>
           </div>
             )}
 
+
             {step === 1 && (
-          <div className="coin-holding-layout">
+          <div className={getClass(1)}>
               <label htmlFor="holdings" className="label-holding-coin">How much coin are you holding?
-              <input type="number" name="holdings" placeholder="Coin amount" id="holdings" required/></label>
+              <input type="number" name="holdings" placeholder="Coin amount" id="holdings" /></label>
               <div className="btn-section">
               <button onClick={prevStep} className="go-back-btn"><i className="fa-solid fa-chevron-left"></i> back </button>
               <button onClick={nextStep} className="move-from-holdings">Next <i className="fa-solid fa-chevron-right"></i></button>
               </div>
           </div>
-            )}
+                )}
 
             {step === 2 && (
-            <div className="coin-price-layout">
+            <div className={getClass(2)}>
                 <div className="price-update">
                     <span className="price-alert">Your <code>coin</code> current price is now <code>$2000</code></span>
                 </div>
@@ -45,10 +60,11 @@ function App() {
                   <button onClick={nextStep} className="move-from-holdings">Next <i className="fa-solid fa-chevron-right"></i></button>
               </div>
             </div>
-            )}
+                )}
+
 
             {step === 3 && (
-            <div className="coin-threshold-layout">
+            <div className={getClass(3)}>
                 <label htmlFor="up-threshold" className="label-threshold-up">
                     Enter the Upper price threshold:
                     <input type="number" name="up-threshold" placeholder="0.5000" id="up-threshold"/>
@@ -63,28 +79,29 @@ function App() {
               <button onClick={nextStep} className="move-from-holdings">Next <i className="fa-solid fa-chevron-right"></i></button>
               </div>
             </div>
-            )}
+                )}
+
 
             {step === 4 && (
-            <div className="holder-email-layout">
+            <div className={getClass(4)}>
                 <label htmlFor="user-email" className="label-email">Enter your email & password
-                    <input type="email" name="user-email" placeholder="a@example.com" id="user-email" aria-autocomplete="inline" required/>
+                    <input type="email" name="user-email" placeholder="a@example.com" id="user-email" aria-autocomplete="inline" />
                 </label>
                 <label htmlFor="user-password" className="label-password">
-                    <input type="password" name="user-password" placeholder="Password" id="user-password" required/>
+                    <input type="password" name="user-password" placeholder="Password" id="user-password" />
                 </label>
                 <label htmlFor="user-confirm-pass" className="label-confirm-pass">
-                    <input type="password" name="user-confirm-pass" placeholder="Confirm password" id="user-confirm-pass" required/>
+                    <input type="password" name="user-confirm-pass" placeholder="Confirm password" id="user-confirm-pass" />
                 </label>
                 <div className="btn-section">
                 <button onClick={prevStep} className="go-back-btn"><i className="fa-solid fa-chevron-left"></i> back </button>
               <button onClick={nextStep} className="move-from-holdings">Next <i className="fa-solid fa-chevron-right"></i></button>
               </div>
             </div>
-            )}
+                )}
 
             {step === 5 && (
-            <div className="code-authenticator-layout">
+            <div className={getClass(5)}>
                 <label htmlFor="code-input" className="code-label">
                     Please input the 4-digit code we sent to your email.
                     <input type="text" name="code-input" id="code-input" placeholder="0000" />
@@ -96,8 +113,8 @@ function App() {
                   </div>
 
             </div>
-            )}
-        </form>
+                )}
+        </div>
       </div>
     </header>
   );
